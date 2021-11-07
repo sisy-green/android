ec070328cdd20e0d41f6f87088a7b52cb30eaeee
tmible <panevnikita@inbox.ru>
2021-11-07 22:58:10 +0300

    Просмотр отдельной картинки по клику

--- app/src/main/java/ru/bmstu/iu9/andruxa/kartinki/MainActivity.kt
+++ app/src/main/java/ru/bmstu/iu9/andruxa/kartinki/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
@@ -15,10 +16,14 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberImagePainter
 import coil.size.OriginalSize
 import kotlinx.coroutines.launch
@@ -32,11 +37,18 @@ import ru.bmstu.iu9.andruxa.kartinki.ui.theme.KartinkiTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = MainViewModel()
         setContent {
             KartinkiTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MainList()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "list") {
+                        composable("list") { MainList(navController, viewModel) }
+                        composable("image/{imageId}") { backStackEntry ->
+                            ImageViewer(backStackEntry.arguments?.getString("imageId"), viewModel)
+                        }
+                    }
                 }
             }
         }
@@ -45,7 +57,6 @@ class MainActivity : ComponentActivity() {
 
 data class ImagesSearchModel(
     val data: List<ImageModel>,
-
 ) {
     class ImageModel(
         val id: String,
@@ -88,7 +99,7 @@ interface ApiService {
     }
 }
 
-class MainListViewModel: ViewModel() {
+class MainViewModel: ViewModel() {
     val images = mutableStateListOf<Image>()
 
     init {
@@ -104,8 +115,8 @@ class MainListViewModel: ViewModel() {
 }
 
 @Composable
-fun MainList() {
-    val images = remember { MainListViewModel().images }
+fun MainList(navController: NavController, viewModel: MainViewModel) {
+    val images = remember { viewModel.images }
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -116,17 +127,32 @@ fun MainList() {
             itemContent = { Image(
                 painter = rememberImagePainter(it.asset.url, builder = { size(OriginalSize) }),
                 contentDescription = it.description,
-                modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .clickable { navController.navigate("image/${it.id}") },
                 contentScale = ContentScale.FillWidth,
             )},
         )
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    KartinkiTheme {
-        MainList()
+fun ImageViewer(id: String?, viewModel: MainViewModel) {
+    id?.let {
+        val image = remember { viewModel.images.find { it.id == id }}
+        image?.let {
+            Box(
+                modifier = Modifier.fillMaxWidth().fillMaxHeight().background(Color(R.color.black)),
+                contentAlignment = Alignment.Center,
+            ) {
+                Image(
+                    painter = rememberImagePainter(it.asset.url, builder = { size(OriginalSize) }),
+                    contentDescription = it.description,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Fit,
+                )
+            }
+        }
     }
 }
