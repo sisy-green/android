4f4a20e4258ef9ffcd1dd73b98026c4f2a2b6859
tmible <panevnikita@inbox.ru>
2021-11-09 23:10:41 +0300

    Можно поделиться картинкой

--- app/src/main/java/ru/bmstu/iu9/andruxa/kartinki/MainActivity.kt
+++ app/src/main/java/ru/bmstu/iu9/andruxa/kartinki/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.bmstu.iu9.andruxa.kartinki
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,16 +10,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -75,21 +78,42 @@ fun MainList(navController: NavController, viewModel: MainViewModel) {
 fun ImageViewer(id: String?, viewModel: MainViewModel) {
   id?.let {
     val image = remember { viewModel.images.find { it.id == id }}
-    image?.let {
-      Column(
-        modifier = Modifier.fillMaxWidth().fillMaxHeight().background(Color(R.color.black)),
-        verticalArrangement = Arrangement.Center,
+    val context = LocalContext.current
+    image?.let { image ->
+      Scaffold(
+        floatingActionButton = {
+          FloatingActionButton(onClick = {
+            val shareIntent: Intent = Intent().apply {
+              action = Intent.ACTION_SEND
+              putExtra(Intent.EXTRA_TEXT, image.asset)
+              type = "image/*"
+            }
+            startActivity(context, Intent.createChooser(shareIntent, "Поделиться"), null)
+          }) {
+            Icon(Icons.Default.Share, contentDescription = "share")
+          }
+        }
       ) {
-        Image(
-          painter = rememberImagePainter(it.asset, builder = { size(OriginalSize) }),
-          contentDescription = it.description,
-          modifier = Modifier.fillMaxWidth(),
-          contentScale = ContentScale.FillWidth,
-        )
-        Text(
-          text = it.description,
-          modifier = Modifier.fillMaxWidth().padding(start = 8.dp, top = 8.dp, end = 8.dp),
-        )
+        Column(
+          modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color(R.color.black)),
+          verticalArrangement = Arrangement.Center,
+        ) {
+          Image(
+            painter = rememberImagePainter(image.asset, builder = { size(OriginalSize) }),
+            contentDescription = image.description,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
+          )
+          Text(
+            text = image.description,
+            modifier = Modifier
+              .fillMaxWidth()
+              .padding(start = 8.dp, top = 8.dp, end = 8.dp),
+          )
+        }
       }
     }
   }
