6d4a766ca38f3f616cdc23642aa581cb633cee14
tmible <panevnikita@inbox.ru>
2021-12-03 02:57:02 +0300

    добавил сохранение состояния для списка картинок, но у них там баг, так что не работает

--- app/src/main/java/ru/bmstu/iu9/andruxa/kartinki/components/ImageList.kt
+++ app/src/main/java/ru/bmstu/iu9/andruxa/kartinki/components/ImageList.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,10 +34,12 @@ fun ImageList(
     onRefresh = { viewModel.updateImages(ImagesSearchParams(categoryID, sort, query)) },
   ) {
     NetworkError(viewModel.imagesError) {
-      Loading(images.size == 0) {
+      Loading(images.isEmpty()) {
+        val state = rememberLazyListState()
         LazyColumn(
           modifier = Modifier.fillMaxWidth(),
           horizontalAlignment = Alignment.CenterHorizontally,
+          state = state,
         ) {
           items(
             items = images,
